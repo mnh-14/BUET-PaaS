7fca019ca5527dc46da08aa071ebc00dd687f6d7
MD. Nafis Hussain <mnh.nafis14@gmail.com>
2026-09-05 11:26:05 +0600

    Added some echos in build scripts

--- deployer/docker-build.sh
+++ deployer/docker-build.sh
@@ -1,4 +1,7 @@
 IMAGE_NAME="192.168.128.152/paas-builders/deploy-service-image"
 IMAGE_TAG="$1"
+echo "Building image: $IMAGE_NAME:$IMAGE_TAG and $IMAGE_NAME:latest"
 docker build -t "$IMAGE_NAME:$IMAGE_TAG" "$IMAGE_NAME:latest" .
+echo "Pushing image to registry: $IMAGE_NAME:$IMAGE_TAG and $IMAGE_NAME:latest"
 docker push "$IMAGE_NAME:$IMAGE_TAG" "$IMAGE_NAME:latest"
+echo "Image build and push complete for: $IMAGE_NAME:$IMAGE_TAG and $IMAGE_NAME:latest"
